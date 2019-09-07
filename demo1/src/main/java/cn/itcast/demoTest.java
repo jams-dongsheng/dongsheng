75788baf245651523dd0cf76e05a1e38288356d3
liuzk <liuzk@163.com>
2019-09-07 15:56:39 +0800

    刘周坤第二次测试

--- demo1/src/main/java/cn/itcast/demoTest.java
+++ demo1/src/main/java/cn/itcast/demoTest.java
@@ -5,5 +5,7 @@ public class demoTest {
         System.out.println("你好");
         System.out.println("何师傅是色情狂");
         System.out.println("好开心");
+        System.out.println("上传成功");
+
     }
 }
