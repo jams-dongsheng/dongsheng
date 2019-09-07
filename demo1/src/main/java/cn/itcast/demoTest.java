6721db746fbe4fbdeef3122939c2c683e5708e05
dongsheng <1367872538@qq.com>
2019-09-07 15:40:48 +0800

    冬昇第一次测试

--- demo1/src/main/java/cn/itcast/demoTest.java
+++ demo1/src/main/java/cn/itcast/demoTest.java
@@ -4,5 +4,6 @@ public class demoTest {
     public static void main(String[] args) {
         System.out.println("你好");
         System.out.println("何师傅是色情狂");
+        System.out.println("好开心");
     }
 }
